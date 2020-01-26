dc6d88ef441835cd8ee03a7d4056c7d8c9b493b3
SHC <shahar.haramati@gmail.com>
2020-01-26 20:37:13 +0200

    env

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'prefix' },
+  { path: 'login', component: LoginComponent },
+  // { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
